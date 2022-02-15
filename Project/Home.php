5b13aebadaaf837f0e009473810a25d53d61628d
Tahsina4287 <muttakitahsina@gmail.com>
2022-02-15 23:34:55 +0600

    bootstrap5-navbar with Js

--- Project/Home.php
+++ Project/Home.php
@@ -6,6 +6,8 @@
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
 
@@ -15,19 +17,16 @@
 
 <body>
 
-    <nav class="navbar navbar-default">
-
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="Home.php">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  
+    <div class="container">
+    <a class="navbar-brand" href="Home.php">
                     <img src="Pictures/hosse.png">
                 </a>
+            <div class="navbar-header">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#bs-example-navbar-collapse-1" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
@@ -45,7 +44,9 @@
                     <li><a href="admin.html"><i class="fa fa-lock"></i> Admin</a></li>
                 </ul>
             </div>
-    </nav>
+            </div>
+    
+</nav>
     <div class="container" style="margin-top: 170px;">
         <form action="" method="GET">
 
@@ -67,8 +68,6 @@
     <div class="row mx-3 my-5">
         <div class="col-xs-1 col-sm-1 col-md-2 col-lg-2"></div>
         <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8">
-            <div class="card" style="margin-top: 10px;">
-                <div class="card-body">
 
 
 
@@ -81,6 +80,9 @@
                         if (mysqli_num_rows($query_run) > 0) {
                             foreach ($query_run as $items) {
                     ?>
+
+<div class="card" style="margin-top: 10px;">
+                <div class="card-body">
                                 <div class="row mt-4"></div>
 
                                 <div class="col-md-3">
@@ -88,7 +90,7 @@
                                         <div class="card" align="center" style="margin-bottom: 15px;">
                                             <img src="<?php echo $items['Pic'] ?>" height="200px" alt="">
                                             <div align="center">
-                                                <h3  style="margin-top: 10px;"><i class="fas fa-map-marker-alt"></i> <?php echo $items['togo'] ?></h3>
+                                                <h3  style="margin-top: 10px;"><i class="	fa fa-map-marker"></i> <?php echo $items['togo'] ?></h3>
                                                 <h4 style="margin: 0px;">Start From: <?php echo $items['StartP'] ?> </h4><br>
                                                 <div class="price">
                                                     <h4 style="margin-top: 0px;"><?php echo $items['COST'] ?> BDT Per Person</h4>
@@ -107,7 +109,7 @@
                                 </div>
                         <?php
                             }
-                        }
+                       
                         ?>
 
 
@@ -116,7 +118,7 @@
 
 
             <?php
-                    } else {
+                    } else{
             ?>
                 <div class="col-xs-1 col-sm-1 col-md-2 col-lg-2"></div>
                 <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8">
@@ -129,6 +131,7 @@
                     </table>
                 <?php
                     }
+                }
                 ?>
 
 
@@ -140,6 +143,8 @@
 
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
 </body>
 
 </html>
